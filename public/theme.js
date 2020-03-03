18eb2e5661d3fd109073242be1e818e2fd7b9876
Bob <alteredorange@gmail.com>
2020-03-03 15:01:23 -0700

    Update theme.js - Local Storage Check
    Added Local Storage Check for theme.js

--- public/theme.js
+++ public/theme.js
@@ -1,19 +1,23 @@
 const themeMap = {
-  dark: 'light',
-  light: 'solar',
-  solar: 'dark'
+    dark: "light",
+    light: "solar",
+    solar: "dark"
 };
 
-const theme = localStorage.getItem('theme');
-const bodyClass = document.body.classList;
-theme && bodyClass.add(theme);
+if (localStorage.theme) {
+    const theme = localStorage.getItem("theme");
+    const bodyClass = document.body.classList;
+    theme && bodyClass.add(theme);
+    function toggleTheme() {
+        const current = localStorage.getItem("theme");
+        const next = themeMap[current];
 
-function toggleTheme() {
-  const current = localStorage.getItem('theme');
-  const next = themeMap[current];
-
-  bodyClass.replace(current, next);
-  localStorage.setItem('theme', next);
+        bodyClass.replace(current, next);
+        localStorage.setItem("theme", next);
+    }
+} else {
+    localStorage.setItem("theme", "light");
 }
 
-document.getElementById('themeButton').onclick = toggleTheme;
+document.getElementById("themeButton").onclick = toggleTheme;
+
